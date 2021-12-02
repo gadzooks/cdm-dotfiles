5b752dc2db047e6dd9feb8879fc44ab2a726f300
akarwande <akarwande@coredigital.com>
2021-12-01 20:37:40 -0800

    fzf files

---
+++ .fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/akarwande/homebrew/Cellar/fzf/0.28.0/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/akarwande/homebrew/Cellar/fzf/0.28.0/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/akarwande/homebrew/Cellar/fzf/0.28.0/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/akarwande/homebrew/Cellar/fzf/0.28.0/shell/key-bindings.bash"
